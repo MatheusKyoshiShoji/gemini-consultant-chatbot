2fc89ec49bd3e4ec5d33d9f8256ea691b84e59c7
Matheus Shoji <142830615+MatheusKyoshiShoji@users.noreply.github.com>
2025-03-22 17:03:47 -0300

    Chat components

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,3 +1,24 @@
+let messages = [];
+const chatContainer = document.querySelector('chat-container');
+
+function insertGeminiMessage(geminiMessageContent) {
+  messages.push(geminiMessageContent);
+}
+
+function chatContentRendering() {
+  let chatContent = "";
+
+  for (const m of messages) {
+      chatContent += `
+        <div class="${m.isUser ? 'chat-bubble-user' : 'chat-bubble'}">
+          <p>${m.message}</p>
+        </div>
+      `
+  }
+
+  chatContainer.innerHTML = chatContent;
+}
+
 inptPromptValue = (element) => {
   if (event.key === "Enter") {
     const prompt = JSON.stringify(element.value);
